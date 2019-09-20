071f0ee18620f525706fddd0ea0b6fa5ed2cd7e2
Romain Séailles <r.seailles@gmail.com>
2019-09-20 08:34:37 +0200

    Debut de gestion avec icalendar

--- ICalendar.py
+++ ICalendar.py
@@ -1,6 +1,8 @@
 import requests
 import datetime
 from bs4 import BeautifulSoup
+from icalendar import Calendar, Event
+
 
 # 1) Construction de l'URL où se trouve l'edt
 n = 1  # Modifiable, nombre de semaines à prendre à l'avance
@@ -11,11 +13,21 @@ urls = [
 
 # 2) Extraction des données
 # Class contenant un évènement
-
-
 class evenement:
-    def __init__(self, date):
-        self.date = date
+    def __init__(self, date, annee):
+        self.event = Event()
+        jour, mois = date.split(' ')[1:]
+        dictMois = {"janvier": 1, "février": 2, "mars": 3, "avril": 4, "mai": 5, "juin": 6, "juillet": 7, "août" :8, "septembre" : 9, "octobre" : 10, "novembre" : 11, "décembre" : 12}
+        self.date = datetime.datetime(day = int(jour), month = dictMois[mois], year = annee)
+        print(date)
+        print(self.date)
+
+    def ICS(self):
+        """Méthode qui retourne sous string le texte à mettre dans le fichier ICS"""
+        pass
+
+    def Date(self, date):
+        self.event.add("date", )
 
     def HeureDebut(self, heureDebut):
         self.heureDebut = heureDebut
@@ -41,11 +53,12 @@ allEvenements = []
 for url in urls:
     soup = BeautifulSoup(requests.get(url).text, features="html.parser")
     jours = soup.findAll("div", {"class": "timetable-day-wrapper"})
+    annee = int(soup.find("div", {"class": "timetable-week-title"}).split(' ')[-1]) #Probleme
     for jour in jours:
         date = jour.find("div", {"class": "timetable-day-title"}).text
         allCours = jour.findAll("div", {'class': "timetable-day-body"})
         for cours in allCours:
-            e = evenement(date)
+            e = evenement(date, annee)
             e.HeureDebut(cours.find(
                 "div", {"class": "timetable-day-time-start"}).text)
             e.HeureFin(cours.find(
@@ -62,6 +75,8 @@ for url in urls:
                 comment += groupe.find("div",
                                        {"class": "timetable-session-room"}).text
                 comment += "\n"
+
             e.Groupes(comment)
             allEvenements.append(e)
-            e.Print()
+
+#3) On va a présent créer le fichier et ajouter les évnénements
